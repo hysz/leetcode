28622a980c5ceac13e4a9de70fda93dc90c30309
Greg Hysen <greg.hysen@gmail.com>
2020-07-16 23:16:17 -0700

    condensed a bit

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -11,54 +11,41 @@ class ListNode:
         return "%d -> "%(self.val) + str(self.next)
 
 class Solution:
+
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
         l1_idx = 0
         l2_idx = 0
         carry = 0
         first = None
         prev = None
-        while l1 and l2:
-            sum = l1.val + l2.val + carry
+        while l1 or l2:
+            sum = (l1.val if l1 else 0) + (l2.val if l2 else 0) + carry
             remainder = sum % 10
             carry = int(sum / 10)
             node = ListNode(remainder, None)
+
+            # Set prev.next/first to this node, if necessary.
             if prev:
                 prev.next = node
             else:
                 first = node
-            prev = node
-            l1 = l1.next
-            l2 = l2.next
-            
-        # Handle case where l1 has more digits than l2
-        while l1:
-            sum = l1.val + carry
-            remainder = sum % 10
-            carry = int(sum / 10)
-            node = ListNode(remainder, None)
-            if prev:
-                prev.next = node
-            prev = node
-            l1 = l1.next
 
-        # Handle case where l2 has more digits than l1
-        while l2:
-            sum = l2.val + carry
-            remainder = sum % 10
-            carry = int(sum / 10)
-            node = ListNode(remainder, None)
-            if prev:
-                prev.next = node
+            # Update `prev` to be this node
             prev = node
-            l2 = l2.next
+
+            # Iterate on l1/l2
+            if l1:
+                l1 = l1.next
+
+            if l2:
+                l2 = l2.next
             
         # Handle carry over at end
         if carry:
-            # can skip some checks; if carry is set then we've already set `prev` and `first`
+            # If carry isset then first/prev isset.
             prev.next = ListNode(carry, None)
                 
         return first
-        print(first)
 
 
 
@@ -66,7 +53,7 @@ class Solution:
 a = ListNode(2, ListNode(4, ListNode(3, None)))
 b = ListNode(5, ListNode(6, ListNode(4, None)))
 print(Solution().addTwoNumbers(a, b))
-'''       
+'''     
 
 '''
 a = ListNode(9, None)
