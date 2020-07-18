a8fa4641ee91f24a0ea429f24ab2a1e210478ab6
Greg Hysen <greg.hysen@gmail.com>
2020-07-17 22:15:16 -0700

    ALL TESTS PASSING, BUT CONTAINER PROBLEM IS EXCEEDING TIME

--- container_with_most_water.py
+++ container_with_most_water.py
@@ -1,11 +1,14 @@
 from typing import List
 import math
+import json
+from pprint import pprint
 
 class Solution:
     
     def maxArea(self, heights: List[int]) -> int:
         max_area = 0
         max_x2 = len(heights) - 1
+        #total_iterations = 0
         
         for x1,y1 in enumerate(heights):
             #print("(%d,%d)"%(x1,y1))
@@ -18,16 +21,19 @@ class Solution:
                 continue
 
             # lets try to beat the max area
-            min_delta_x_to_beat_max_area = min(1, math.ceil(max_area / y1))
-            for x2 in range(max_x2, x1 + (min_delta_x_to_beat_max_area - 1), -1):
+            min_x2_to_beat_max_area = x1 + max(1, math.ceil(float(max_area) / float(y1)))
+            x2 = max_x2
+            while x2 >= min_x2_to_beat_max_area:
                 # check if any future iteration could do better
-                if (x2 - x1) * y1 <= max_area:
-                    break
-
+                #total_iterations += 1
                 area = (x2 - x1) * min(y1, heights[x2])
                 if area > max_area:
                     max_area = area
+                    min_x2_to_beat_max_area = x1 + max(1, math.ceil(float(max_area) / float(y1)))
 
+                x2 -= 1
+
+        #print("Total iterations = %d", total_iterations)
         return max_area
 
 '''
