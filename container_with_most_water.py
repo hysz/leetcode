60f3c53d0fc91aa5de5875e93b59c2a1c39adda0
Greg Hysen <greg.hysen@gmail.com>
2020-07-17 20:30:26 -0700

    solved their thing

--- container_with_most_water.py
+++ container_with_most_water.py
@@ -1,5 +1,6 @@
 import collections
 from typing import List
+import math
 
 Point = collections.namedtuple('Point', ['x','y'])
 
@@ -12,22 +13,43 @@ class Solution:
         # We've passed our best remaining case when:
         #    getArea(p1, Point(x2, p1.y)) <= max_area
         #    => (p2.x - p1.x) * p1.y <= max_area
-        return (x2 - p1.x) * p1.y <= max_area
+        return 
     
     def maxArea(self, heights: List[int]) -> int:
         max_area = 0
         
+        max_x2 = len(heights) - 1
         for x1,y1 in enumerate(heights):
             p1 = Point(x1, y1)
             x2 = len(heights) - 1
-            for x2 in range(len(heights) - 1, x1, -1):
-                if Solution.cannotBeatMaxArea(max_area, p1, x2):
+
+            # no area when height is zero
+            if y1 == 0:
+                continue
+
+            # first check to see if we cannot beat max area
+            min_delta_x_to_beat = math.ceil(max_area / y1)
+            if max_x2 - x1 < min_delta_x_to_beat:
+                print("break1")
+                continue
+
+            for x2 in range(max_x2, x1 + min_delta_x_to_beat - 1, -1):
+                if (x2 - p1.x) * p1.y <= max_area:
+                    print("Break2")
                     break
                     
                 volume = Solution.getArea(p1, Point(x2, heights[x2]))
                 if volume > max_area:
+                    print("Max area: [%d..%d] = %d"%(x1, x2, volume))
                     max_area = volume
         
         return max_area
-        
+
+'''
 print(Solution().maxArea([1,8,6,2,5,4,8,3,7]))
+print(Solution().maxArea([1]))
+print(Solution().maxArea([]))
+print(Solution().maxArea([0]))
+'''
+
+print(Solution().maxArea([1,3,2,5,25,24,5]))
