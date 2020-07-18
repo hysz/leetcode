79b06a6fb6cb167edddccb6e404b5ba1a90242e2
Greg Hysen <greg.hysen@gmail.com>
2020-07-17 21:54:20 -0700

    still time limit exceeded

--- container_with_most_water.py
+++ container_with_most_water.py
@@ -1,17 +1,11 @@
 from typing import List
 import math
-import json
-from pprint import pprint
 
 class Solution:
     
     def maxArea(self, heights: List[int]) -> int:
         max_area = 0
         max_x2 = len(heights) - 1
-        j = 0
-        total_iterations = 0
-        for x1,y1 in enumerate(heights):
-            j = 1
         
         for x1,y1 in enumerate(heights):
             #print("(%d,%d)"%(x1,y1))
@@ -20,34 +14,20 @@ class Solution:
                 continue
 
             # first check to see if we can even beat max area
-            #print("Check %d <= %d"%((max_x2 - x1) * y1, max_area) )
             if (max_x2 - x1) * y1 <= max_area:
                 continue
 
             # lets try to beat the max area
-            min_delta_x_to_beat = min(1, math.ceil(max_area / y1))
-            for x2 in range(max_x2, x1 + (min_delta_x_to_beat - 1), -1):
-                if x2 == x1:
-                    raise Exception("WAHT")
-
+            min_delta_x_to_beat_max_area = min(1, math.ceil(max_area / y1))
+            for x2 in range(max_x2, x1 + (min_delta_x_to_beat_max_area - 1), -1):
                 # check if any future iteration could do better
                 if (x2 - x1) * y1 <= max_area:
                     break
 
-                
-
-                total_iterations += 1
                 area = (x2 - x1) * min(y1, heights[x2])
                 if area > max_area:
                     max_area = area
 
-                    
-            #print("#iterations = %d"%(iterations))
-            
-            if x1 == 100:
-                break
-        
-        pprint("Total Iterations = %d"%(total_iterations))
         return max_area
 
 '''
@@ -55,7 +35,7 @@ print(Solution().maxArea([1,8,6,2,5,4,8,3,7]))
 print(Solution().maxArea([1]))
 print(Solution().maxArea([]))
 print(Solution().maxArea([0]))
-'''
+
 
 
 print(Solution().maxArea([1,3,2,5,25,24,5]))
@@ -63,4 +43,3 @@ print(Solution().maxArea([1,3,2,5,25,24,5]))
 
 '''
 print(Solution().maxArea(json.load(open('./ar.json'))))
-'''
