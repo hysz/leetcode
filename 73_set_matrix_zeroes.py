908a6d3af37c40d727cd9cab0353c1ed31144e91
Greg Hysen <greg.hysen@gmail.com>
2020-07-18 22:30:12 -0700

    did first part, splitting the thing up

--- 73_set_matrix_zeroes.py
+++ 73_set_matrix_zeroes.py
@@ -15,31 +15,52 @@ class Solution:
         min_i = 0
         j = 0
         min_j = 0
+
+        print("MAtrix is %dx%d"%(len(matrix), len(matrix[0])))
         
+
+        # First move the rows up
+        while i < len(matrix):
+            j = 0
+            while j < len(matrix[0]):
+                if matrix[i][j] == 0:
+                    # swap each cell of current row with that in `min_i`,
+                    # storing the current row index in each cell of the current row
+                    for k in range(0, len(matrix[0])):
+                        matrix[i][k] = matrix[min_i][k]
+
+                        if matrix[min_i][k] != 0:
+                            matrix[min_i][k] = i if i > 0 else -1
+                        
+                    min_i += 1
+
+                    # Hit up the next row
+                    break
+                j += 1
+            i += 1
+
+        # Next move the columns over
+        i = 0
+        j = 0
         while i < len(matrix):
             print("i: ", i)
-            j = 0
+            j = min_j
             while j < len(matrix[0]):
                 print("j: ", j)
                 if matrix[i][j] == 0:
-                    print("Yo")
-                    # swap each cell of current row with that in `min_i`,
-                    # storing the current row index in each cell of the current row
-                    for k in range(min_j, len(matrix[0])):
-                        matrix[i][k] = matrix[min_i][k]
-                        matrix[min_i][k] = j
-                        
-                    min_i += 1
-                    
+                    print("FOUND AT (%d, %d)"%(i,j))
+
                     # swap each cell of current column with that in `min_j`,
                     # storing the current row index in each cell of the current row
                     for k in range(min_i, len(matrix)):
                         matrix[k][j] = matrix[k][min_j]
-                        matrix[k][min_j] = i
-                        
+                        matrix[k][min_j] = j
+
                     min_j += 1
                 j += 1
             i += 1
+
+
 
         
 
@@ -57,5 +78,6 @@ matrix = [
   [3,4,5,2],
   [1,3,1,5]
 ]
+print(matrix)
 Solution().setZeroes(matrix)
 pprint(matrix)
