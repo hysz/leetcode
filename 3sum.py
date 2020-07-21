8d495b5b415db6916e25ff8e49c315cbec4327e5
Greg Hysen <greg.hysen@gmail.com>
2020-07-20 20:38:20 -0700

    311/313 on time!

--- 3sum.py
+++ 3sum.py
@@ -2,50 +2,56 @@ from typing import List
 from collections import defaultdict
 
 class Solution:
-    exists = None
-    
-    def getCombinations(self, nums: List[int], size: int) -> List[int]:
-        if size == 0:
-            return []
-        elif size == 1:
-            return [[n] for n in nums]
-        
-        exists = defaultdict(lambda: defaultdict(lambda: defaultdict(bool)))
+   
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
         combos = []
-        for i,n in enumerate(nums):
-            n_combos = self.getCombinations(nums[(i+1):], size - 1)
-            
-            # Aggregate if we're not at the top-level of recursion (inferred by size=3)
-            if size != 3:
-                combos += [[n] + n_combo for n_combo in n_combos]
-                continue
-            
-            # This is where we aggregate the combos with 3 elements
-            for n_combo in n_combos:
-                # Check if this combo equals zero.
-                n2 = n_combo[0]
-                n3 = n_combo[1]
-                if n + n2 != -n3:
-                    continue
-                
-                # Check if we've already recorded this combo.
-                if self.exists[n][n2][n3]:
-                    continue
-                self.exists[n][n2][n3] = True
+        exists = defaultdict(lambda: defaultdict(lambda: defaultdict(bool)))
+        for i in range(0, len(nums)):
+            ni = nums[i]
+            for j in range(i+1, len(nums)):
+                nj = nums[j]
+                for k in range(j+1, len(nums)):
+                    nk = nums[k]
+                    # Check if this combo equals zero.
+                    if ni + nj != -nk:
+                        continue
 
-                # Woot, we found a unique combo that sums to zero!
-                combos.append([n,n2,n3])
+                    # Sort our `n` values
+                    n1 = ni
+                    n2 = nj
+                    n3 = nk
+                    if n3 < n2:
+                        t = n2
+                        n2 = n3
+                        n3 = t
+
+                    if n2 < n1:
+                        t = n1
+                        n1 = n2
+                        n2 = t
+
+                        if n3 < n2:
+                            t = n2
+                            n2 = n3
+                            n3 = t
+                    
+                    # Check if we've already recorded this combo.
+                    if exists[n1][n2][n3]:
+                        continue
+                    exists[n1][n2][n3] = True
+
+                    # Woot, we found a unique combo that sums to zero!
+                    combos.append([n1,n2,n3])
 
         return combos
-    
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        self.exists = defaultdict(lambda: defaultdict(lambda: defaultdict(bool)))
-        return self.getCombinations(sorted(nums), 3)
 
 #print(Solution.getCombinations([1,2,3,4], 2))
 
-print(Solution().threeSum([-1,0,1]))
+#print(Solution().threeSum([-1,0,1]))
 
 #print(Solution().threeSum([-1, 0, 1, 2, -1, -4]))
 #print(Solution().threeSum([0,8,2,-9,-14,5,2,-5,-5,-9,-1,3,1,-8,0,-3,-12,2,11,9,13,-14,2,-15,4,10,9,7,14,-8,-2,-1,-15,-15,-2,8,-3,7,-12,8,6,2,-12,-8,1,-4,-3,5,13,-7,-1,11,-13,8,4,6,3,-2,-2,3,-2,3,9,-10,-4,-8,14,8,7,9,1,-2,-3,5,5,5,8,9,-5,6,-12,1,-5,12,-6,14,3,5,-11,8,-7,2,-12,9,8,-1,9,-1,-7,1,-7,1,14,-3,13,-4,-12,6,-9,-10,-10,-14,7,0,13,8,-9,1,-2,-5,-14]))
 #print(Solution().threeSum([7,5,-8,-6,-13,7,10,1,1,-4,-14,0,-1,-10,1,-13,-4,6,-11,8,-6,0,0,-5,0,11,-9,8,2,-6,4,-14,6,4,-5,0,-12,12,-13,5,-6,10,-10,0,7,-2,-5,-12,12,-9,12,-9,6,-11,1,14,8,-1,7,-13,8,-11,-11,0,0,-1,-15,3,-11,9,-7,-10,4,-2,5,-4,12,7,-8,9,14,-11,7,5,-15,-15,-4,0,0,-11,3,-15,-15,7,0,0,13,-7,-12,9,9,-3,14,-1,2,5,2,-9,-3,1,7,-12,-3,-1,1,-2,0,12,5,7,8,-7,7,8,7,-15]))
+#print(Solution().threeSum([13,-5,3,3,-1,13,3,1,-9,-4,9,12,6,-9,-6,-12,-8,3,12,14,4,-15,2,-11,4,-12,10,9,-6,-3,-8,14,7,3,2,-8,-7,-10,8,-8,-7,-6,-11,6,-7,-2,9,-8,8,-2,13,-10,-2,0,-14,-13,-4,11,3,-3,-7,3,-4,8,13,13,-15,-9,10,0,-2,-12,1,2,9,1,8,-4,8,-7,9,7,-2,-15,14,0,-13,-13,-12,-2,-1,-11,8,10,12,6,8,4,12,3,11,-12,-2,-3,5,-15,6,-10,-4,-1,-1,-10,13]))
+print(Solution().threeSum([3,0,-2,-1,1,2]))
+#print(len(Solution().threeSum([1,2,3,4,5,6,7,8])))
