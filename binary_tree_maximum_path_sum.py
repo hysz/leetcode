daf0d54303d046251ad6be4f4db8eed8a825ca78
Greg Hysen <greg.hysen@gmail.com>
2020-07-22 22:19:48 -0700

    wrapped up binary_tree_maximum_path_sum.py with comments

--- binary_tree_maximum_path_sum.py
+++ binary_tree_maximum_path_sum.py
@@ -11,10 +11,23 @@ class TreeNode:
         self.left = left
         self.right = right
 
-Stats = namedtuple('Sats', ['single', 'combined', 'max_local', 'max_global'])
+
+# At any node we have four options:
+#   1. Route the longest path to the left.
+#   2. Route the longest path to the right.
+#   3. Halt the longest path at the current node.
+#   4. Merge the left and right paths.
+# We store stats below to represent these cases.
+# extend - max value of cases 1-3 above. The parent will use its child's `extend` stat to extend their path downward.
+# merge - value from case 4 above. This means that the path starts from the left, goes up to this node, then extends down the right.
+# max_local - this is the max of extend and merge -- namely, what is the best option at this point?
+# max_global - this is the global maximum path that we've found.
+Stats = namedtuple('Sats', ['extend', 'merge', 'max_local', 'max_global'])
 
 class Solution:
 
+    # We work our way up from the bottom of the tree. At each step we record a bunch
+    # of stats. See above for definitions.
     def run(self, node) -> Stats:
         if node == None:
             return Stats(0, 0, 0, -(pow(2,31)))
@@ -23,19 +36,14 @@ class Solution:
         left = self.run(node.left)
         right = self.run(node.right)
 
-
-        #print(left)
-        #print(right)
-
         # compute local stats
-        single = max(node.val, node.val + max(left.single, right.single))
-        combined = node.val + left.single + right.single
-        max_local = sorted([single, combined, node.val])[-1]
+        extend = max(node.val, node.val + max(left.extend, right.extend))
+        merge = node.val + left.extend + right.extend
+        max_local = max(extend, merge)
 
         # update global max
         max_global = sorted([max_local, left.max_global, right.max_global])[-1]
-        stats = Stats(single, combined, max_local, max_global)  
-        #print(stats)
+        stats = Stats(extend, merge, max_local, max_global)  
         return stats      
 
     def maxPathSum(self, root: TreeNode) -> int:
