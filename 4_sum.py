44a2941f88dca9ceb4757abfe23a1602b8af067d
Greg Hysen <greg.hysen@gmail.com>
2020-07-23 21:15:50 -0700

    4_sum: passing 175/282

--- 4_sum.py
+++ 4_sum.py
@@ -6,45 +6,27 @@ class Solution:
             return []
 
         nums = sorted(nums)
-        a = 0
-        b = 1
-        c = len(nums) - 2
-        d = len(nums) - 1
-
         combos = []
 
-        while a < b and b < c and c < d:
-            print("[%d,%d,%d,%d]"%(a,b,c,d))
-            total = nums[a] + nums[b] + nums[c] + nums[d]
-            if total < target:
-                if (a < b - 1 and (target - total) >= (nums[b] - nums[a])) or b == c - 1:
-                    a += 1
-                else:
-                    b += 1
-            elif total > target:
-                if (d > c + 1 and (total - target) >= (nums[d] - nums[c])) or b == c - 1:
-                    d -= 1
-                else:
-                    c -= 1
-            else:
-                combos.append([nums[a],nums[b],nums[c],nums[d]])
-                if b != c - 1:
-                    b += 1
-                elif a < b - 1:
-                    a += 1
-                elif d > c + 1:
-                    d -= 1
-                else:
-                     break
-
+        for a in range(0, len(nums) - 3):
+            for b in range(a + 1, len(nums) - 2):
+                search_sum = (target - (nums[a] + nums[b]))
+                c = b + 1
+                d = len(nums) - 1
+                while c != d:
+                    search_sum_candidate = nums[c] + nums[d]
+                    if search_sum_candidate > search_sum:
+                        d -= 1
+                    elif search_sum_candidate < search_sum:
+                        c += 1
+                    else: # they equal! Woot
+                        combos.append([nums[a], nums[b], nums[c], nums[d]])
+                        c += 1
         return combos
 
-            #if b == (c - 1) and a == (b - 1) and c == (d - 1):
-            #    break
 
-
-#print(Solution().fourSum([1, 0, -1, 0, -2, 2], 0))
+print(Solution().fourSum([1, 0, -1, 0, -2, 2], 0))
 #print(Solution().fourSum([0, 0, 0, 0], 1))
-print(Solution().fourSum([-3,-1,0,2,4,5], 2))
+#print(Solution().fourSum([-3,-1,0,2,4,5], 2))
 
 
