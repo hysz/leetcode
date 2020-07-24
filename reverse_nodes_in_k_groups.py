3a33b43868e0e058d7679ff1b64faf95dde7529a
Greg Hysen <greg.hysen@gmail.com>
2020-07-23 23:21:15 -0700

    working on the reverse_nodes_in_k_groups.py

---
+++ reverse_nodes_in_k_groups.py
@@ -0,0 +1,73 @@
+from typing import List
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def __repr__(self):
+        return "%s -> %s"%(self.val, self.next)
+
+class Solution:
+
+    # swap chilren between parents
+    def swapNodes(parent1, parent2):
+        print("parent 1 - ", parent1)
+        print("parent 2 - ", parent2)
+
+        child1 = parent1.next
+        child2 = parent2.next
+
+        print("c1: ", child1)
+        print("c2: ", child2)
+
+        # swap grandchildren
+        child1_next = child1.next
+        child1.next = child2.next
+        child2.next = child1_next if child1_next != child2 else child1
+
+        print("c1': ", child1)
+        print("c2': ", child2)
+
+        # swap children
+        print("parent 1 - ", parent1)
+        parent1.next = child2
+        print("parent1': ", parent1)
+        
+        print("parent 2 - ", parent2)
+        parent2.next = child1 if parent2 != child1 else parent2.next
+        print("parent2': ", parent2)
+        
+    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
+        
+        # seems kinda like 
+        curk = k
+ 
+        
+
+        fakehead = top = ListNode(9999999, head)
+        for i in range(k, 0, -2):
+            print(i)
+            # iterate to the (n-1)th node. We'll alter its `next`
+            
+            node = top
+            while node and i > 1:
+                node = node.next
+                i -= 1
+                
+            # couldn't countdown k
+            if i > 1:
+                break
+                
+            print("stop at %s"%(node))
+
+            Solution.swapNodes(top, node)
+
+            # iterate top
+            top = top.next if top else head.next
+            
+        return fakehead.next
+
+
+print(Solution().reverseKGroup(ListNode(1, ListNode(2, ListNode(3))), 2))
