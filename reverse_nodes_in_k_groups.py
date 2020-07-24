cc1c1b22a1c8bd913a29daef64cb4e7b7dc32d45
Greg Hysen <greg.hysen@gmail.com>
2020-07-24 00:29:21 -0700

    Submitted reverse_nodes_in_k_groups.py -- I like this one!

--- reverse_nodes_in_k_groups.py
+++ reverse_nodes_in_k_groups.py
@@ -11,18 +11,13 @@ class ListNode:
 
 class Solution:
 
-    # swap chilren between parents
-    def swapNodes(parent1, parent2):
-        ##print("parent 1 - ", parent1)
-        ##print("parent 2 - ", parent2)
-
+    # swap children between parents
+    def swapChildren(parent1, parent2):
+        # cache children
         child1 = parent1.next
         child2 = parent2.next
 
-        ##print("c1: ", child1)
-        ##print("c2: ", child2)
-
-        # swap parents -- important to swap parents first!
+        # swap parents -- important to swap parents first, o/w we'll get some recursion below.
         parent1.next = child2
         parent2.next = child1 if parent2 != child1 else parent2.next
 
@@ -36,52 +31,45 @@ class Solution:
         if k == 1:
             return head
         
-        # seems kinda like 
-        curk = k
- 
+        # we need to use parent nodes to swap, so we create a fake head to avoid special-casing the head.
         fakehead = ListNode(9999999, head)
 
-        next_top = fakehead
-        while next_top and next_top.next:
-            top = next_top
-            #print("Starting NYA")
-            #print("-----------")
+        # Run algo
+        next_parent1 = fakehead
+        while next_parent1 and next_parent1.next:
+            parent1 = next_parent1
+
+            # We reverse two at a time, starting at the outside and moving in.
+            # For example (k=2): [1,2,3,4] -> [4,2,3,1] -> [4,3,2,1].
+            # I went with this strategy bc I think it results in the least amount of swaps.
             current_k = k
             while current_k >= 2:
-                #print("TOP: ", top)
-                #print("Current K: ", current_k)
-                # iterate to the (n-1)th node. We'll alter its `next`
-                
-                node = top
-                # find parent of who we'll swap with
+                parent2 = parent1
+                # Find the 2nd parent whose child we'll swap with the child of `parent1`
+                # (because pointers go from parent -> child, we need to find parents, not the actual that will be swapped).
                 for i in range(1, current_k):
-                    node = node.next
-                    if not node:
+                    parent2 = parent2.next
+                    if not parent2:
                         break
                     
-                # couldn't countdown k
-                #print("node ", node)
-                if not node or not node.next:
-                    #print("hmm be we eatin")
-                    next_top = None
+                # Check if we don't have a full k group
+                if not parent2 or not parent2.next:
+                    next_parent1 = None # avoid an infinite loop
                     break
                     
-                ##print("stop at %s"%(node))
+                # Swap 'em children!
+                Solution.swapChildren(parent1, parent2)
 
-                Solution.swapNodes(top, node)
+                # Iterate on `parent1` to swap next most inward pair.
+                parent1 = parent1.next
 
-                # iterate top
-                top = top.next
-
+                # Keep track of who the next parent1 will be (after the current k group)
                 if current_k == k:
-                    #print("WANT HERE: ", node)
-                    next_top = node if k == 2 else node.next
+                    # There's a special case when k=2. Didn't dig too deep.
+                    next_parent1 = parent2 if k == 2 else parent2.next
                 
+                # Decrement k to decrease the distance of the swap on the next iteratio (bc we're moving inward).
                 current_k -= 2
-                
-                #print(fakehead.next)
-                #print("---- end k group -----")
-                
             
         return fakehead.next
 
